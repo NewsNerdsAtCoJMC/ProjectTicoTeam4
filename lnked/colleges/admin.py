7ac21e6b7361b3326f3670706dd7ed99252f8193
raine17 <ecase@huskers.unl.edu>
2017-03-14 11:44:08 -0500

    updated admin.py
    imported and registered models

--- lnked/colleges/admin.py
+++ lnked/colleges/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import SignificantMajors, College, Blog
+
+admin.site.register(SignificantMajors)
+
+admin.site.register(College)
+
+admin.site.register(Blog)
